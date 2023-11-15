cb848a041ea2c76dd399a3b0323c11dc94df6622
Craig Barbisan <craig_barbisan@hotmail.com>
2023-11-15 15:13:57 -0500

    Added role to contractor record
    Added service cut table for configuring percentage of fee
    calculation per contractor/service.
    
    Added role service table to assist with creation of service
    cut data.

--- code/ddl/contractor.sql
+++ code/ddl/contractor.sql
@@ -5,6 +5,7 @@ GO
 CREATE TABLE [dbo].[contractor] (
     [contractor_id] INT IDENTITY(1, 1) NOT NULL,
     [contractor_name] NVARCHAR(200) NOT NULL,
+	[role] NVARCHAR(50) NOT NULL,
     [active] BIT DEFAULT 1 NOT NULL
     CONSTRAINT [PK_contractor] PRIMARY KEY CLUSTERED ([contractor_id] ASC)
 );
@@ -12,4 +13,5 @@ GO
 
 INSERT INTO dbo.contractor
 SELECT	'Leanda Barbisan, OT Reg. (Ont.)' AS contractor_name,
+		'Occupational Therapist' AS role,
 		1 AS active;
