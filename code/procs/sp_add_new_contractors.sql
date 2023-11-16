1cb0067896b5c3993eb4cbebe1a9f8297381eb6a
Craig Barbisan <craig_barbisan@hotmail.com>
2023-11-16 09:14:06 -0500

    New proc for updating the contractor list

---
+++ code/procs/sp_add_new_contractors.sql
@@ -0,0 +1,24 @@
+CREATE OR ALTER PROCEDURE dbo.sp_add_new_contractors
+AS 
+BEGIN
+
+    SET NOCOUNT ON;
+    
+    WITH CONTRACTOR_LIST AS (
+        SELECT  DISTINCT therapist_name AS contractor_name
+        FROM    dbo.owl_session
+        UNION
+        SELECT  DISTINCT supervisor AS contractor_name
+        FROM    dbo.owl_session
+        WHERE   supervisor IS NOT NULL
+    )
+    INSERT INTO dbo.contractor
+    SELECT  contractor_name,
+            'TBD' AS role,
+            1 AS active
+    FROM    CONTRACTOR_LIST cl
+    WHERE   NOT EXISTS (SELECT 1 FROM dbo.contractor WHERE contractor_name = cl.contractor_name);
+
+    PRINT('Please manually update the role for any new contractors added.')
+
+END
