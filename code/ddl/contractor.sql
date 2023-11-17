b1c85b9af44e483e9a891cfbe94c54ac17314a5a
Craig Barbisan <craig_barbisan@hotmail.com>
2023-11-17 12:17:58 -0500

    Added invoice frequency to contractor record and renamed role to department

--- code/ddl/contractor.sql
+++ code/ddl/contractor.sql
@@ -3,15 +3,17 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[contractor] (
-    [contractor_id] INT IDENTITY(1, 1) NOT NULL,
-    [contractor_name] NVARCHAR(200) NOT NULL,
-	[role] NVARCHAR(50) NOT NULL,
-    [active] BIT DEFAULT 1 NOT NULL
+    [contractor_id]     INT            IDENTITY (1, 1) NOT NULL,
+    [contractor_name]   NVARCHAR (200) NOT NULL,
+    [department]        NVARCHAR (50)  NOT NULL,
+    [invoice_frequency] NVARCHAR (50)  CONSTRAINT [DEFAULT_contractor_invoice_frequency] DEFAULT 'Monthly' NOT NULL,
+    [active]            BIT            DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_contractor] PRIMARY KEY CLUSTERED ([contractor_id] ASC)
 );
 GO
 
 INSERT INTO dbo.contractor
 SELECT	'Leanda Barbisan, OT Reg. (Ont.)' AS contractor_name,
-		'Occupational Therapist' AS role,
+		'OCCUPATIONAL THERAPY' AS department,
+        'Monthly' AS invoice_frequency,
 		1 AS active;
