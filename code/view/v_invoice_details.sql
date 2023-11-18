651bbda09b16f9e98f65c559a2f99ae0a9309c73
Craig Barbisan <craig_barbisan@hotmail.com>
2023-11-18 10:54:03 -0500

    Joined payment date into the invoice details view, so it could be used in selection criteria. e.g. If we're selecting relevant details for November invoices, select all records from the view that had a payment in November. Sessions without payments don't get paid.

--- code/view/v_invoice_details.sql
+++ code/view/v_invoice_details.sql
@@ -17,7 +17,14 @@ GO
 CREATE OR ALTER VIEW [dbo].[v_invoice_details]
 WITH SCHEMABINDING
 AS
-SELECT      c.contractor_id,
+WITH SESSION_PAYMENT AS (
+    SELECT      session_id,
+                MIN(payment_date) AS payment_date
+    FROM        dbo.payment
+    GROUP BY    session_id
+)
+SELECT      sp.payment_date,
+            c.contractor_id,
             c.contractor_name,
             s.session_id,
             FORMAT(s.session_date,'yyyy-MM-dd h:mm tt') AS session_date,
@@ -39,11 +46,12 @@ SELECT      c.contractor_id,
 FROM        dbo.contractor c
 JOIN        dbo.owl_session s
 ON          (c.contractor_name = s.therapist_name OR c.contractor_name = s.supervisor)
+JOIN        SESSION_PAYMENT sp
+ON          s.session_id = sp.session_id
 LEFT JOIN   dbo.service_cut_override sc
 ON          c.contractor_id = sc.contractor_id
 AND         s.service_name = sc.service_name
 -- If we filter on note status, then we will have to maintain our session data, or sessions that didn't have a signed
 -- note when they were loaded, will never get added to the invoice!
 --AND         s.note_status IN ('Signed Note', 'N/A')
-WHERE       EXISTS (SELECT 1 FROM dbo.payment WHERE session_id = s.session_id)
 GO
